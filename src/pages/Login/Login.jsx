55cfad57a54fa0b3a080f4c5c1b47e3effe50a80
daochinam <daochinam1081406@gmail.com>
2024-06-26 22:16:43 +0700

    fix loi giao dien

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -38,23 +38,7 @@ export default function Login(props) {
       <div className=" bg-green-100 flex justify-center items-center">
         <div className="p-8 flex-1">
           <div className="w-96 bg-white rounded-3xl mx-auto overflow-hidden shadow-xl">
-            <div className="relative h-24 bg-green-500 rounded-bl-4xl">
-              <svg
-                className="absolute bottom-0"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1440 320"
-              >
-                <path
-                  fill="#ffffff"
-                  fillOpacity={1}
-                  d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-                />
-              </svg>
-            </div>
             <div className="px-10 pt-2 pb-8 bg-white rounded-tr-4xl">
-              <h1 className="text-2xl font-semibold text-gray-900">
-                Welcome back Fiverr !
-              </h1>
               <div className="mt-6">
                 {}
                 <Form.Item
